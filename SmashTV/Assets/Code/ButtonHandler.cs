718443c6b9247a9d86226b56234d2f236c18ce6c
Dkaban <dustinkaban@gmail.com>
2020-09-09 14:24:57 -0700

    Added a pause button and a restart button

---
+++ SmashTV/Assets/Code/ButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿/*************************
+ * 
+ * ButtonHandler.cs
+ * 
+ * Author: Dustin Kaban
+ * Date: September 9th, 2020
+ * 
+ * This class handles all button input functions
+ * 
+ *************************/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    //Pauses the time scale.  NOT THE BEST IDEA, but it works for now.
+    //TODO: Clean this up.
+    public void PressPause()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    //Restarts the scene from scratch
+    public void PressRestart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
