78b90a515cf908d69dd3eec984d7c95659ef88c9
Dustin Kaban <dustinkaban@gmail.com>
2020-09-14 23:11:20 -0700

    Added calculator type functionality in UI test scene

--- SmashTV/Assets/Scenes/TestBed/UITest/UITestHandler.cs
+++ SmashTV/Assets/Scenes/TestBed/UITest/UITestHandler.cs
@@ -10,6 +10,7 @@
  *************************/
 
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,13 +18,16 @@ public class UITestHandler : MonoBehaviour
 {
     public Transform GridParent;
     public GameObject ButtonObject;
-    protected private Boolean _darkMode = false;
+    protected private Boolean _nightMode = false;
+    protected private int _inputValue = 0;
+    public Text InputText;
 
     private void Start()
     {
         AddButtons(9);
     }
 
+    //Create all the buttons for the menu
     private void AddButtons(int buttonCount)
     {
         for(int i=1;i<=buttonCount;i++)
@@ -32,25 +36,45 @@ public class UITestHandler : MonoBehaviour
             Button button = buttonObject.GetComponent<Button>();
             button.GetComponentInChildren<Text>().text = i.ToString();
             buttonObject.transform.SetParent(GridParent,false);
+            ButtonAddListener(button, i);
         }
     }
 
-    public void SetDarkMode()
+    //Toggle between night mode and regular mode
+    public void SetNightMode()
     {
-        switch(_darkMode)
+        switch(_nightMode)
         {
             case true:
                 Camera.main.backgroundColor = new Color(255, 255, 255, 255);
-                _darkMode = false;
+                _nightMode = false;
                 break;
 
             case false:
                 Camera.main.backgroundColor = new Color(0,0,0,0);
-                _darkMode = true;
+                _nightMode = true;
                 break;
 
             default:
                 break;
         }
+    }
+
+    //Add the listener to each button, this has to be done here and not inside the loop
+    //If done inside the loop, listener takes the last value passed.
+    void ButtonAddListener(Button button, int i)
+    {
+        button.onClick.AddListener(() => NumberPressAction(i));
+    }
+
+    void NumberPressAction(int value)
+    {
+        UpdateInputValue(value);
+    }
+
+    private void UpdateInputValue(int value)
+    {
+        _inputValue = value;
+        InputText.text = _inputValue.ToString();
     }
 }
